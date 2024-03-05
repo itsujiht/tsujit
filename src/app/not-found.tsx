b11edf455a4c544a8de8be6f37d4664402d66d0a
nemusheep <miyomayo02@gmail.com>
2024-03-05 17:53:15 +0900

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import '@/app/globals.css';
 export default function Page() {
     return (
         <div className='App-main'>
-            <h1>not found</h1>
+            <h1>404 not found</h1>
             <p>ないよ</p>
         </div>
     );
